7cd0636b2e1ef7224a4a9d1f84dd0b1f1c8eb61e
Dan <dd.subscriptions@gmail.com>
2023-05-15 20:42:41 -0500

    feat(library): add notion api to library page

---
+++ src/_data/libraryLinks.js
@@ -0,0 +1,38 @@
+const Cache = require("@11ty/eleventy-cache-assets");
+require("dotenv").config();
+
+module.exports = async () => {
+  try {
+    const { results } = await Cache(
+      "https://api.notion.com/v1/databases/6e2a35a17b5344aeb5db0fe47cc5764a/query",
+      {
+        duration: "1d", // 1 day
+        type: "json",
+        fetchOptions: {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+            "Notion-Version": "2021-05-13",
+            "Content-Type": "application/json",
+          },
+        },
+      }
+    );
+    const trimmedResults = results.map((result) => {
+      const { properties } = result;
+      const { Title, URL, Notes } = properties;
+      return {
+        created: result.created_time,
+        notes: Notes.rich_text[0]?.text.content,
+        title: Title.title[0]?.plain_text,
+        url: URL.url,
+      };
+    });
+    return trimmedResults;
+  } catch (ex) {
+    console.log(ex);
+
+    // If failed, return back an empty array
+    return [];
+  }
+};
